91e865353c6587ca9a55cb73c4e5089a2ee16be9
benyusuferkan <125639000+benyusuferkan@users.noreply.github.com>
2025-12-04 15:16:24 +0300

    Proje altyapisi ve MVC sablonu olusturuldu
    Proje altyapisi ve MVC sablonu olusturuldu

---
+++ SporSalonuYonetim.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SporSalonuYonetim.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
